b2965b115b9dc097a564208d5aac5b19c6fd8636
mmm999xp <mmm999xp@gmail.com>
2023-07-15 17:33:07 +0800

    add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 // 引入 home 模組程式碼
 const home = require('./modules/home')
 const restaurantRoute = require('./modules/restaurantRoute')
+const users = require('./modules/users')
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 
 router.use('/', home)
 router.use('/', restaurantRoute)
+router.use('/users', users)
 
 
 module.exports = router
